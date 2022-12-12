26cb9175f7f0d948d36f72b1a5c6ddaf02746b9c
dmitriy <dmitriylosevxxx@gmail.com>
2022-12-12 04:09:23 +0300

    Добавлены mappers для запроса CertRes

--- app/src/main/java/aleksandr/fedotkin/buyercryptomoney/data/di/set/mapper/BuyerCertificateMapperModule.kt
+++ app/src/main/java/aleksandr/fedotkin/buyercryptomoney/data/di/set/mapper/BuyerCertificateMapperModule.kt
@@ -3,6 +3,11 @@ package aleksandr.fedotkin.buyercryptomoney.data.di.set.mapper
 import aleksandr.fedotkin.buyercryptomoney.data.mappers.set.certificate.CardCInitReqMapper
 import aleksandr.fedotkin.buyercryptomoney.data.mappers.set.certificate.CardCInitResMapper
 import aleksandr.fedotkin.buyercryptomoney.data.mappers.set.certificate.CardCInitResTBSMapper
+import aleksandr.fedotkin.buyercryptomoney.data.mappers.set.certificate.RegFormReqDataMapper
+import aleksandr.fedotkin.buyercryptomoney.data.mappers.set.certificate.certres.CAMsgMapper
+import aleksandr.fedotkin.buyercryptomoney.data.mappers.set.certificate.certres.CertResDataMapper
+import aleksandr.fedotkin.buyercryptomoney.data.mappers.set.certificate.certres.CertStatusMapper
+import aleksandr.fedotkin.buyercryptomoney.data.mappers.set.certificate.certres.FailedItemMapper
 import org.koin.dsl.module
 
 val buyerCertificateMapperModule = module {
@@ -25,6 +30,38 @@ val buyerCertificateMapperModule = module {
         CardCInitResTBSMapper(
             byteArrayMapper = get(),
             bigIntegerMapper = get()
+        )
+    }
+
+    factory {
+        RegFormReqDataMapper(
+            bigIntegerMapper = get(),
+            byteArrayMapper = get()
+        )
+    }
+
+    factory {
+        FailedItemMapper()
+    }
+
+    factory {
+        CAMsgMapper()
+    }
+
+    factory {
+        CertStatusMapper(
+            byteArrayMapper = get(),
+            keyRepository = get(),
+            failedItemMapper = get(),
+            caMsgMapper = get()
+        )
+    }
+
+    factory {
+        CertResDataMapper(
+            bigIntegerMapper = get(),
+            byteArrayMapper = get(),
+            certStatusMapper = get()
         )
     }
 }
