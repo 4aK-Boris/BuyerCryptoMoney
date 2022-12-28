627b73c8dbcb780e2144e6459ee519b96baf2919
dmitriy <dmitriylosevxxx@gmail.com>
2022-12-29 01:32:00 +0300

    Мапперы на запрос CardCInitRes

--- SetProtocol/src/main/java/aleksandr/fedotkin/set/protocol/data/mappers/certificate/card/c/init/res/CardCInitResTBSMapper.kt
+++ SetProtocol/src/main/java/aleksandr/fedotkin/set/protocol/data/mappers/certificate/card/c/init/res/CardCInitResTBSMapper.kt
@@ -1,36 +1,41 @@
 package aleksandr.fedotkin.set.protocol.data.mappers.certificate.card.c.init.res
 
-import aleksandr.fedotkin.set.protocol.data.mappers.core.BigIntegerMapper
-import aleksandr.fedotkin.set.protocol.data.mappers.core.ByteArrayMapper
+import aleksandr.fedotkin.set.protocol.core.mapper.SetMapper
 import aleksandr.fedotkin.set.protocol.data.dto.certificate.card.c.init.res.CardCInitResTBS
+import aleksandr.fedotkin.set.protocol.data.mappers.core.Base64Mapper
+import aleksandr.fedotkin.set.protocol.data.mappers.core.BigIntegerMapper
 import aleksandr.fedotkin.set.protocol.domain.models.certificate.card.c.init.res.CardCInitResTBSModel
+import kotlinx.serialization.KSerializer
 
 class CardCInitResTBSMapper(
-    private val byteArrayMapper: ByteArrayMapper,
+    private val base64Mapper: Base64Mapper,
     private val bigIntegerMapper: BigIntegerMapper
-) {
+): SetMapper<CardCInitResTBSModel, CardCInitResTBS> {
 
-    fun map(dto: CardCInitResTBS): CardCInitResTBSModel {
-        return CardCInitResTBSModel(
-            rrpID = bigIntegerMapper.map(string = dto.rrpID),
-            lidEE = bigIntegerMapper.map(string = dto.lidEE),
-            challEE = bigIntegerMapper.map(string = dto.challEE),
-            lidCA = bigIntegerMapper.map(string = dto.lidCA),
-            caeThumb = byteArrayMapper.map(string = dto.caeThumb),
-            brandCRLIdentifier = dto.brandCRLIdentifier.map { byteArrayMapper.map(string = it) },
-            thumbs = dto.thumbs.map { byteArrayMapper.map(string = it) }
+    override val serializer: KSerializer<CardCInitResTBS>
+        get() = CardCInitResTBS.serializer()
+
+    override fun map(value: CardCInitResTBSModel): CardCInitResTBS {
+        return CardCInitResTBS(
+            rrpID = bigIntegerMapper.map(value = value.rrpID),
+            lidEE = bigIntegerMapper.map(value= value.lidEE),
+            challEE = bigIntegerMapper.map(value = value.challEE),
+            lidCA = bigIntegerMapper.map(value = value.lidCA),
+            caeThumb = base64Mapper.map(value = value.caeThumb),
+            brandCRLIdentifier = value.brandCRLIdentifier.map { base64Mapper.map(value = it) },
+            thumbs = value.thumbs.map { base64Mapper.map(value = it) }
         )
     }
 
-    fun map(model: CardCInitResTBSModel): CardCInitResTBS {
-        return CardCInitResTBS(
-            rrpID = bigIntegerMapper.map(number = model.rrpID),
-            lidEE = bigIntegerMapper.map(number= model.lidEE),
-            challEE = bigIntegerMapper.map(number = model.challEE),
-            lidCA = bigIntegerMapper.map(number = model.lidCA),
-            caeThumb = byteArrayMapper.map(byteArray = model.caeThumb),
-            brandCRLIdentifier = model.brandCRLIdentifier.map { byteArrayMapper.map(byteArray = it) },
-            thumbs = model.thumbs.map { byteArrayMapper.map(byteArray = it) }
+    override fun reverseMap(value: CardCInitResTBS): CardCInitResTBSModel {
+        return CardCInitResTBSModel(
+            rrpID = bigIntegerMapper.reverseMap(value = value.rrpID),
+            lidEE = bigIntegerMapper.reverseMap(value = value.lidEE),
+            challEE = bigIntegerMapper.reverseMap(value = value.challEE),
+            lidCA = bigIntegerMapper.reverseMap(value = value.lidCA),
+            caeThumb = base64Mapper.reverseMap(value = value.caeThumb),
+            brandCRLIdentifier = value.brandCRLIdentifier.map { base64Mapper.reverseMap(value = it) },
+            thumbs = value.thumbs.map { base64Mapper.reverseMap(value = it) }
         )
     }
 }
