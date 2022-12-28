c9f540df180694f8b0fb1824556e2d6370d14cdd
dmitriy <dmitriylosevxxx@gmail.com>
2022-12-29 01:24:47 +0300

    Сертификаты для сертификационного центра

--- SetProtocol/src/main/java/aleksandr/fedotkin/set/protocol/core/di/SetModule.kt
+++ SetProtocol/src/main/java/aleksandr/fedotkin/set/protocol/core/di/SetModule.kt
@@ -1,5 +1,6 @@
 package aleksandr.fedotkin.set.protocol.core.di
 
+import aleksandr.fedotkin.set.protocol.core.di.certificate.certificateModule
 import aleksandr.fedotkin.set.protocol.data.di.crypto.cryptoModule
 import aleksandr.fedotkin.set.protocol.data.di.mapper.mappersModule
 import aleksandr.fedotkin.set.protocol.data.di.repository.repositoriesModule
@@ -8,5 +9,5 @@ import aleksandr.fedotkin.set.protocol.domain.di.useCasesModule
 import org.koin.dsl.module
 
 val setModule = module {
-    includes(repositoriesModule, mappersModule, cryptoModule, useCasesModule, setNetworkModule)
+    includes(repositoriesModule, mappersModule, cryptoModule, useCasesModule, setNetworkModule, certificateModule)
 }
