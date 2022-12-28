4997175496f285df83c5913a3906b17f8131f4dc
dmitriy <dmitriylosevxxx@gmail.com>
2022-12-29 01:32:17 +0300

    Мапперы на запрос RegFormReq

--- SetProtocol/src/main/java/aleksandr/fedotkin/set/protocol/data/mappers/certificate/reg/form/req/RegFormReqDataMapper.kt
+++ SetProtocol/src/main/java/aleksandr/fedotkin/set/protocol/data/mappers/certificate/reg/form/req/RegFormReqDataMapper.kt
@@ -1,36 +1,41 @@
 package aleksandr.fedotkin.set.protocol.data.mappers.certificate.reg.form.req
 
-import aleksandr.fedotkin.set.protocol.data.mappers.core.BigIntegerMapper
-import aleksandr.fedotkin.set.protocol.data.mappers.core.ByteArrayMapper
+import aleksandr.fedotkin.set.protocol.core.mapper.SetMapper
 import aleksandr.fedotkin.set.protocol.data.dto.certificate.reg.form.req.RegFormReqData
+import aleksandr.fedotkin.set.protocol.data.mappers.core.Base64Mapper
+import aleksandr.fedotkin.set.protocol.data.mappers.core.BigIntegerMapper
 import aleksandr.fedotkin.set.protocol.domain.models.certificate.reg.form.req.RegFormReqDataModel
+import kotlinx.serialization.KSerializer
 
 class RegFormReqDataMapper(
     private val bigIntegerMapper: BigIntegerMapper,
-    private val byteArrayMapper: ByteArrayMapper
-) {
+    private val base64Mapper: Base64Mapper
+) : SetMapper<RegFormReqDataModel, RegFormReqData> {
 
-    fun map(model: RegFormReqDataModel): RegFormReqData {
+    override val serializer: KSerializer<RegFormReqData>
+        get() = RegFormReqData.serializer()
+
+    override fun map(value: RegFormReqDataModel): RegFormReqData {
         return RegFormReqData(
-            rrpID = bigIntegerMapper.map(number = model.rrpID),
-            lidEE = bigIntegerMapper.map(number = model.lidEE),
-            challEE2 = bigIntegerMapper.map(number = model.challEE2),
-            lidCA = bigIntegerMapper.map(number = model.lidCA),
-            requestType = model.requestType,
-            language = model.language,
-            thumbs = model.thumbs.map { byteArrayMapper.map(byteArray = it) }
+            rrpID = bigIntegerMapper.map(value = value.rrpID),
+            lidEE = bigIntegerMapper.map(value = value.lidEE),
+            challEE2 = bigIntegerMapper.map(value = value.challEE2),
+            lidCA = bigIntegerMapper.map(value = value.lidCA),
+            requestType = value.requestType,
+            language = value.language,
+            thumbs = value.thumbs.map { base64Mapper.map(value = it) }
         )
     }
 
-    fun map(dto: RegFormReqData): RegFormReqDataModel {
+    override fun reverseMap(value: RegFormReqData): RegFormReqDataModel {
         return RegFormReqDataModel(
-            rrpID = bigIntegerMapper.map(string = dto.rrpID),
-            lidEE = bigIntegerMapper.map(string = dto.lidEE),
-            challEE2 = bigIntegerMapper.map(string = dto.challEE2),
-            lidCA = bigIntegerMapper.map(string =dto.lidCA),
-            requestType = dto.requestType,
-            language = dto.language,
-            thumbs = dto.thumbs.map { byteArrayMapper.map(string = it) }
+            rrpID = bigIntegerMapper.reverseMap(value = value.rrpID),
+            lidEE = bigIntegerMapper.reverseMap(value = value.lidEE),
+            challEE2 = bigIntegerMapper.reverseMap(value = value.challEE2),
+            lidCA = bigIntegerMapper.reverseMap(value = value.lidCA),
+            requestType = value.requestType,
+            language = value.language,
+            thumbs = value.thumbs.map { base64Mapper.reverseMap(value = it) }
         )
     }
 }
