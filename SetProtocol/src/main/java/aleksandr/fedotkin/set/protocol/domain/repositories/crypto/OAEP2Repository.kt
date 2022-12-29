60154735873d87876ee99501d368ecaf34eae9cb
dmitriy <dmitriylosevxxx@gmail.com>
2022-12-30 00:04:13 +0300

    Добавлен репозитории для зашифрованного конверта OAEP

--- SetProtocol/src/main/java/aleksandr/fedotkin/set/protocol/domain/repositories/crypto/OAEP2Repository.kt
+++ SetProtocol/src/main/java/aleksandr/fedotkin/set/protocol/domain/repositories/crypto/OAEP2Repository.kt
@@ -1,44 +1,17 @@
 package aleksandr.fedotkin.set.protocol.domain.repositories.crypto
 
-//interface OAEP2Repository {
-//
-//    fun <T: Model, R: DTO> convertToModel(
-//        oaep2: OAEP2,
-//        map: (R) -> T,
-//        serializer: KSerializer<R>
-//    ): OAEP2Model<T>
-//
-//    fun <T: Model, R: DTO> convertToDTO(
-//        oaep2Model: OAEP2Model<T>,
-//        map: (T) -> R,
-//        serializer: KSerializer<R>
-//    ): OAEP2
-//
-//    suspend fun <T : Model, R : DTO> createAndEncrypt(
-//        secretKey: SecretKey,
-//        data: T,
-//        certificate: X509Certificate,
-//        map: (T) -> R,
-//        serializer: KSerializer<R>
-//    ): ByteArray
-//
-//    suspend fun <T : Model, R : DTO> encrypt(
-//        model: OAEP2Model<T>,
-//        certificate: X509Certificate,
-//        map: (T) -> R,
-//        serializer: KSerializer<R>): ByteArray
-//
-//    suspend fun <T : Model, R : DTO> encrypt(
-//        model: OAEP2Model<T>,
-//        publicKey: PublicKey,
-//        map: (T) -> R,
-//        serializer: KSerializer<R>
-//    ): ByteArray
-//
-//    suspend fun <T : Model, R : DTO> decrypt(
-//        data: ByteArray,
-//        privateKey: PrivateKey,
-//        map: (R) -> T,
-//        serializer: KSerializer<R>
-//    ): OAEP2Model<T>
-//}
+import aleksandr.fedotkin.set.protocol.core.repository.BaseSetRepository
+import aleksandr.fedotkin.set.protocol.data.dto.crypto.oaep.OAEP2
+import aleksandr.fedotkin.set.protocol.domain.models.crypto.oaep.OAEP2Model
+import java.security.PrivateKey
+import java.security.PublicKey
+import javax.crypto.SecretKey
+
+interface OAEP2Repository: BaseSetRepository<OAEP2Model, OAEP2> {
+
+    suspend fun create(secretKey: SecretKey, hash: ByteArray): OAEP2Model
+
+    suspend fun encrypt(model: OAEP2Model, publicKey: PublicKey): ByteArray
+
+    suspend fun decrypt(data: ByteArray, privateKey: PrivateKey): OAEP2Model
+}
--- SetProtocol/src/main/java/aleksandr/fedotkin/set/protocol/domain/repositories/crypto/OAEP3Repository.kt
+++ SetProtocol/src/main/java/aleksandr/fedotkin/set/protocol/domain/repositories/crypto/OAEP3Repository.kt
@@ -1,45 +1,19 @@
 package aleksandr.fedotkin.set.protocol.domain.repositories.crypto
 
-//interface OAEP3Repository {
-//
-//    suspend fun <T: Model> createOAEPModel(
-//        secretKey: SecretKey,
-//        hash: ByteArray,
-//        p: T
-//    ): OAEP3Model<T>
-//
-//    suspend fun <T: Model, R: DTO> createAndEncryptOAEPModel(
-//        secretKey: SecretKey,
-//        hash: ByteArray,
-//        p: T,
-//        map: (T) -> R,
-//        serializer: KSerializer<R>,
-//        publicKey: PublicKey
-//    ): ByteArray
-//
-//    suspend fun <T: Model, R: DTO> encryptOAEPModel(
-//        oaepModel: OAEP3Model<T>,
-//        map: (T) -> R,
-//        serializer: KSerializer<R>,
-//        publicKey: PublicKey
-//    ): ByteArray
-//
-//    suspend fun <T: Model, R: DTO> decryptOAEPModel(
-//        cipherOAEP: ByteArray,
-//        privateKey: PrivateKey,
-//        serializer: KSerializer<R>,
-//        map: (R) -> T
-//    ): OAEP3Model<T>
-//
-//    fun <T: Model, R: DTO> convertToModel(
-//        oaep: OAEP3,
-//        map: (R) -> T,
-//        serializer: KSerializer<R>
-//    ): OAEP3Model<T>
-//
-//    fun <T: Model, R: DTO> convertToDTO(
-//        oaepModel: OAEP3Model<T>,
-//        map: (T) -> R,
-//        serializer: KSerializer<R>
-//    ): OAEP3
-//}
+import aleksandr.fedotkin.set.protocol.core.DTO
+import aleksandr.fedotkin.set.protocol.core.Model
+import aleksandr.fedotkin.set.protocol.core.repository.BaseSetRepository
+import aleksandr.fedotkin.set.protocol.data.dto.crypto.oaep.OAEP3
+import aleksandr.fedotkin.set.protocol.domain.models.crypto.oaep.OAEP3Model
+import java.security.PrivateKey
+import java.security.PublicKey
+import javax.crypto.SecretKey
+
+interface OAEP3Repository<T: Model, R: DTO>: BaseSetRepository<OAEP3Model<T>, OAEP3<R>> {
+
+    suspend fun create(secretKey: SecretKey, data: T): OAEP3Model<T>
+
+    suspend fun encrypt(model: OAEP3Model<T>, publicKey: PublicKey): ByteArray
+
+    suspend fun decrypt(data: ByteArray, privateKey: PrivateKey): OAEP3Model<T>
+}
