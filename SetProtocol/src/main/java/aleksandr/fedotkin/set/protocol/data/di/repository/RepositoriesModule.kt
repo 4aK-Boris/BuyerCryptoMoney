156dbc66ec4868c5e8407b5a621e3c453cde43c5
dmitriy <dmitriylosevxxx@gmail.com>
2022-12-30 01:39:00 +0300

    Репозитории для запроса RegFormReq

--- SetProtocol/src/main/java/aleksandr/fedotkin/set/protocol/data/di/repository/RepositoriesModule.kt
+++ SetProtocol/src/main/java/aleksandr/fedotkin/set/protocol/data/di/repository/RepositoriesModule.kt
@@ -1,40 +1,10 @@
 package aleksandr.fedotkin.set.protocol.data.di.repository
 
-import aleksandr.fedotkin.set.protocol.core.di.certificate.Certificate
-import aleksandr.fedotkin.set.protocol.core.di.certificate.CertificateQualifiers
-import aleksandr.fedotkin.set.protocol.data.mappers.certificate.card.c.init.res.CardCInitResTBSMapper
-import aleksandr.fedotkin.set.protocol.data.repositories.certificate.card.c.init.req.CardCInitReqRepositoryImpl
-import aleksandr.fedotkin.set.protocol.data.repositories.certificate.card.c.init.res.CardCInitResRepositoryImpl
-import aleksandr.fedotkin.set.protocol.data.repositories.certificate.card.c.init.res.CardCInitResTBSRepositoryImpl
-import aleksandr.fedotkin.set.protocol.domain.repositories.certificate.card.c.init.req.CardCInitReqRepository
-import aleksandr.fedotkin.set.protocol.domain.repositories.certificate.card.c.init.res.CardCInitResRepository
-import aleksandr.fedotkin.set.protocol.domain.repositories.certificate.card.c.init.res.CardCInitResTBSRepository
-import org.koin.core.parameter.parametersOf
-import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val repositoriesModule = module {
 
-    includes(coreRepositoriesModule, errorRepositoriesModule, messageWrapperRepositoryModule, cryptoRepositoriesModule)
-
-    factory<CardCInitReqRepository> { CardCInitReqRepositoryImpl(mapper = get()) }
-
-    factory<CardCInitResTBSRepository> {
-        CardCInitResTBSRepositoryImpl(
-            mapper = get(),
-            keyRepository = get(),
-            privateKey = get(qualifier = named(CertificateQualifiers.CCA)),
-            certificate = get<Certificate>(qualifier = named(CertificateQualifiers.CCA)).x509Certificate,
-            signatureRepository = get { parametersOf(get<CardCInitResTBSMapper>()) }
-        )
-    }
-
-    factory<CardCInitResRepository> {
-        CardCInitResRepositoryImpl(
-            mapper = get(),
-            cardCInitResTBSRepository = get()
-        )
-    }
+    includes(coreRepositoriesModule, errorRepositoriesModule, messageWrapperRepositoryModule, cryptoRepositoriesModule, certificateRepositoryModule)
 
 //
 //    factory<ErrorRepository> {
--- SetProtocol/src/main/java/aleksandr/fedotkin/set/protocol/domain/repositories/certificate/reg/form/req/RegFormReqRepository.kt
+++ SetProtocol/src/main/java/aleksandr/fedotkin/set/protocol/domain/repositories/certificate/reg/form/req/RegFormReqRepository.kt
@@ -1,18 +1,27 @@
 package aleksandr.fedotkin.set.protocol.domain.repositories.certificate.reg.form.req
 
-interface RegFormReqRepository {
+import aleksandr.fedotkin.set.protocol.core.repository.BaseSetRepository
+import aleksandr.fedotkin.set.protocol.data.dto.certificate.reg.form.req.RegFormReq
+import aleksandr.fedotkin.set.protocol.domain.models.certificate.reg.form.req.PANOnlyModel
+import aleksandr.fedotkin.set.protocol.domain.models.certificate.reg.form.req.RegFormReqDataModel
+import aleksandr.fedotkin.set.protocol.domain.models.certificate.reg.form.req.RegFormReqModel
+import java.math.BigInteger
+import java.security.PrivateKey
+import java.security.PublicKey
 
-//    val convertToModel: (RegFormReqData) -> RegFormReqDataModel
-//
-//    val convertToDTO: (RegFormReqDataModel) -> RegFormReqData
-//
-//    val serializer: KSerializer<RegFormReqData>
-//
-//    suspend fun createRegFormReqDataModel(lidEE: BigInteger, lidCA: BigInteger): Pair<RegFormReqDataModel, BigInteger>
-//    suspend fun createCryptoDataModel(
-//        number: String,
-//        lidEE: BigInteger,
-//        lidCA: BigInteger,
-//        caeThumb: ByteArray
-//    ): Pair<CryptoDataModel, BigInteger>
+interface RegFormReqRepository : BaseSetRepository<RegFormReqModel, RegFormReq> {
+
+    suspend fun create(
+        rrpid: BigInteger,
+        lidEE: BigInteger,
+        lidCA: BigInteger,
+        number: String,
+        publicKey: PublicKey
+    ): RegFormReqModel
+
+    suspend fun decryptAndCheck(
+        rrpid: BigInteger,
+        privateKey: PrivateKey,
+        model: RegFormReqModel
+    ): Pair<RegFormReqDataModel, PANOnlyModel>
 }
